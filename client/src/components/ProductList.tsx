68a08c5eda2cd51ae8cd32d32749d1919c9a49c9
PedroRomaoDev <pedro7ntj@gmail.com>
2025-12-15 13:26:19 -0300

    style: columns changed to 3 for style

--- client/src/components/ProductList.tsx
+++ client/src/components/ProductList.tsx
@@ -118,7 +118,7 @@ const ProductList = () => {
   return (
     <div className="w-full ">
       <Categories />
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-12">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-3 gap-12">
         {products.map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
