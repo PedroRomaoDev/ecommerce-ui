c4e564e447db7c979006abaaa25f9b0ca4b782e7
PedroRomaoDev <pedro7ntj@gmail.com>
2025-12-17 18:01:52 -0300

    style: optimize layout width for better screen utilization

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <div className="mx-auto p-4 sm:px-0 sm:max-w-xl md:max-w-2xl lg:max-w-3xl xl:max-7xl ">
+        <div className="mx-auto p-4 sm:px-0 sm:max-w-xl md:max-w-2xl lg:max-w-3xl xl:max-w-3xl 2xl:max-w-4xl">
           <Navbar />
           {children}
           <Footer />
